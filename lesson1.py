e4ebafcd42bced616f40a7766ff4071522bd4d5e
Roman <97549564+Romchodes@users.noreply.github.com>
2023-01-17 23:04:25 +0300

    first lessons
    First steps in python learning. The beginning of my programming

---
+++ lesson1.py
@@ -0,0 +1,3 @@
+"""documentation for the first lesson"""
+if 5 > 2:
+    print("Пять больше двух!") #otstupi vazhni
---
+++ lesson2.py
@@ -0,0 +1,13 @@
+X = 'X'
+x = '7'
+cifra = '3'
+x = x + X
+print(X, x, cifra)
+
+x = 'the best'
+print('Python ' + x)
+
+a = 3
+b = 7
+c = a + b
+print(c)
