95e359e6f253dc1009d8f6710ae817ae1e0938dd
Roman <97549564+romchodes@users.noreply.github.com>
2023-01-22 00:09:48 +0300

    add lesson6

--- lesson6.py
+++ lesson6.py
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 a = 3
 b = 7
 c = 9
@@ -14,4 +15,22 @@ print(y)
 print(float(w))
 print(float(v))
 print(float(u))
+=======
+a = 3
+b = 7
+c = 9
+z = a + b
+x = a - b
+y = a * b
+w = b / a
+v = b % a
+u = b // a
+q = b ** a
+print(z)
+print(x)
+print(y)
+print(float(w))
+print(float(v))
+print(float(u))
+>>>>>>> d165fc6... add lesson6
 print(q)
