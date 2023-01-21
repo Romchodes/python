d165fc6a1461eba2473b47492add3ef5435cb4ac
Roman <97549564+Romchodes@users.noreply.github.com>
2023-01-22 00:09:48 +0300

    add lesson6

---
+++ lesson6.py
@@ -0,0 +1,17 @@
+a = 3
+b = 7
+c = 9
+z = a + b
+x = a - b
+y = a * b
+w = b / a
+v = b % a
+u = b // a
+q = b ** a
+print(z)
+print(x)
+print(y)
+print(float(w))
+print(float(v))
+print(float(u))
+print(q)
